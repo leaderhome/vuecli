fbf7e74d0b87a66a91dacc487bb84b2bbb7dae13
chenxianglei <chenxianglei@mobanker.com>
2018-04-03 14:25:45 +0800

    1.0.0

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+//css
+require('@/assets/styles/mui.css');
+require('@/assets/styles/iconfont.css');
+
+//js
+window.mui = require('@/assets/libs/mui.js');
+window.utils = require('@/assets/libs/utils.js').utils;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
+
+/* 自定义指令 */
+Vue.directive('colorSwatch', {
+  inserted(el, binding, vnode) {
+    el.style.backgroundColor = binding.value.bgColor;
+  }
+})
---
+++ src/views/login/login.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+    <p>dddddddddddddd</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'login'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
